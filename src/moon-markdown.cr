07a4b670a928869a67ac878702409df7675f034d
kkpagaev <kkpagaev@gmail.com>
2023-03-28 17:47:17 +0300

    c

--- src/moon-markdown.cr
+++ src/moon-markdown.cr
@@ -2,10 +2,3 @@ require "./parser/*"
 require "./types"
 require "./builder"
 require "./parser"
-
-# TODO: Write documentation for `Moon::Markdown`
-module Moon::Markdown
-  VERSION = "0.1.0"
-
-  # TODO: Put your code here
-end
